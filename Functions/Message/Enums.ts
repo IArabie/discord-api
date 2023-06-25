fa0f1a75d08dc329faa7c7b80c9447339fdba641
TOBZi <iiarabieii@gmail.com>
2023-06-25 20:58:53 +0200

    Create Enums.ts

---
+++ Functions/Message/Enums.ts
@@ -0,0 +1,6 @@
+export interface MessageOptions {
+    content?: string;
+    files?: string[];
+}
+
+export const Token: string = 'MTEwNjAwNTQ0ODk0OTI1MjIyNg.GeCy9c.NzFktfFG5QJj59iDiTcBHE6Pb2IEhqTlwkAFiI';
